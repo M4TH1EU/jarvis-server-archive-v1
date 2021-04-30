2686456068d08589aae0bd27fc0a1a63df4f4392
Mathieu <m4th1eu@protonmail.com>
2021-04-30 15:58:48 +0200

    Avancement sentence (not finished)

--- sentences.py
+++ sentences.py
@@ -1,5 +1,5 @@
 import random
-
+import csv
 import openpyxl
 # from main import speak
 import pyttsx3
@@ -7,59 +7,96 @@ import pyttsx3
 import homeassistant.lights
 import homeassistant.spotify
 
-wb = openpyxl.load_workbook('Sentences.xlsx')
-sheet = wb.get_sheet_by_name('Sentences')
+sentences = {}
 
-for x in range(1, 9):
-    print(x, sheet.cell(row=1, column=1).value)
+def registerSentences():
+    # our csv file and a csv reader (encoded in utf8 for special character compatibility)
+    csv_file = open('Sentences.csv', encoding="utf-8")
+    csv_reader = csv.reader(csv_file, delimiter='|')
+
+    # column number and id(s)
+    column = 0
+    sentencesId = csv_file.readline().split("|")
+
+    # for every line in our .csv file
+    for lines in csv_reader:
+
+        # for every hotwords in our .csv file (based on first line)
+        for sentenceId in sentencesId:
+
+            # if there some empty "fake" column then don't try to register them
+            if 'Column' in sentenceId:
+                break
+            else:
+                # fix an issue with first id
+                sentenceId = sentenceId.replace('\ufeff', '')
+
+                # creating an entry to be able to use .append afterward
+                if sentenceId not in sentences:
+                    sentences[sentenceId] = []
+
+                # if the entry is not empty then add it to the sentences dict
+                if lines[column] != "":
+                    sentences[sentenceId].append(lines[column])
+
+            # increment column
+            column = column + 1
+
+        # reset column
+        column = 0
 
 
-def answer(answers):
-    speak(random.choice(answers))
-    print(random.choice(answers))
+def checkAndAnswer(sentence, sentence_id):
+    if sentence in getSentences(sentence_id):
+        speak(random.choice(getSentences(sentence_id)))
+        print(random.choice(getSentences(sentence_id)))
+
+
+def getSentences(sentence_id):
+    return sentences[sentence_id]
 
 
 def recogniseSentence(sentence):
     # hey john
-    if sentence in hotwordDetection:
-        answer(yesSir)
+    if sentence in getSentences('hotwordDetection'):
+        checkAndAnswer('hotwordDetection')
 
     # comment ça va
     elif sentence in howAreYouDetection:
-        answer(allGoodSir)
+        checkAndAnswer(allGoodSir)
 
     # allume la lumière
     elif sentence in turnOnLightsDetection:
-        answer(turningOnLights)
+        checkAndAnswer(turningOnLights)
         homeassistant.lights.turnOn("light.lumieres_chambre")
 
     # éteint la lumière
     elif sentence in turnOffLightsDetection:
-        answer(turningOffLights)
+        checkAndAnswer(turningOffLights)
         homeassistant.lights.turnOff("light.lumieres_chambre")
 
     # allume les leds
     elif sentence in turnOnLedsDetection:
-        answer(turningOffLights)
+        checkAndAnswer(turningOffLights)
         homeassistant.lights.turnOn("light.leds_chambre")
 
     # éteint les leds
     elif sentence in turnOffLedsDetection:
-        answer(turningOffLights)
+        checkAndAnswer(turningOffLights)
         homeassistant.lights.turnOff("light.leds_chambre")
 
     # mets le morceau suivant
     elif sentence in nextTrackDetection:
-        answer(nextTrack)
+        checkAndAnswer(nextTrack)
         homeassistant.spotify.nextTrack("media_player.spotify_mathieu_broillet")
 
     # mets le morceau précédent
     elif sentence in previousTrackDetection:
         homeassistant.spotify.nextTrack("media_player.spotify_mathieu_broillet")
-        answer(previousTrack)
+        checkAndAnswer(previousTrack)
 
     else:
-        answer(dontUnderstand)
+        checkAndAnswer(dontUnderstand)
 
 
 def speak(text):
