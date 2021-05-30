f974a324e5e00fa719db90a5b2542245299430b8
Mathieu <m4th1eu@protonmail.com>
2021-05-30 16:35:07 +0200

    Fixed intents utils methods with new intents system

--- intents/intents.py
+++ intents/intents.py
@@ -28,6 +28,13 @@ def get_all_intents():
         return all_intents
 
 
+def get_matching_intent_for_tag(tag):
+    for intent in get_all_intents():
+        if intent.get('tag') == tag:
+            return intent
+    raise Exception("Intent for tag " + tag + " not found!")
+
+
 def get_random_response_for_tag(tag):
     """
     Return a random sentence using it's tag.
@@ -40,35 +47,29 @@ def get_random_response_for_tag(tag):
     -------
 
     """
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            return random.choice(intent['responses'])
 
-    return "none"
+    intent = get_matching_intent_for_tag(tag)
+    return random.choice(intent.get('responses'))
 
 
 def get_list_of_patterns_for_tag(tag):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            return intent['patterns']
-    return "none"
+    intent = get_matching_intent_for_tag(tag)
+    return intent.get('patterns')
 
 
 def get_random_from_list_for_tag(tag, list_name):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            if list_name in intent:
-                return random.choice(intent[list_name])
-    return "none"
+    intent = get_matching_intent_for_tag(tag)
+    if list_name in intent:
+        return random.choice(intent.get(list_name))
 
 
 def does_tag_has_service(tag):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            if intent.get('service') is not None:
-                return True
+    intent = get_matching_intent_for_tag(tag)
 
-    return False
+    if intent.get('service') is not None:
+        return True
+    else:
+        return False
 
 
 def get_tag_service(tag):
@@ -83,36 +84,38 @@ def get_tag_service(tag):
     str
     """
 
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            return intent['service']
+    intent = get_matching_intent_for_tag(tag)
+    return intent.get('service')
 
 
 def get_from_intent_for_tag(field_name, tag):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            if intent.get(field_name) is not None:
-                return intent[field_name]
+    intent = get_matching_intent_for_tag(tag)
+
+    if intent.get(field_name) is not None:
+        return intent.get(field_name)
+
     return None
 
 
 def get_from_data_for_tag(field_name, tag):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            data = intent.get('data')
-            if data is not None:
-                if data.get(field_name) is not None:
-                    return data.get(field_name)
+    intent = get_matching_intent_for_tag(tag)
+
+    data = intent.get('data')
+    if data is not None:
+        if data.get(field_name) is not None:
+            return data.get(field_name)
+
     return None
 
 
 def get_data_for_tag(tag):
-    for intent in intents['intents']:
-        if intent['tag'] == tag:
-            if intent.get('data') is not None:
-                return intent.get('data')
-            else:
-                entity_id = get_from_data_for_tag('entity_id', tag)
-                if entity_id is not None:
-                    return {"entity_id": entity_id}
-    return {}
+    intent = get_matching_intent_for_tag(tag)
+
+    if intent.get('data') is not None:
+        return intent.get('data')
+    else:
+        entity_id = get_from_data_for_tag('entity_id', tag)
+        if entity_id is not None:
+            return {"entity_id": entity_id}
+
+    return dict()
